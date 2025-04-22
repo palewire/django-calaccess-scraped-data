db25f3baca2c70b921f088fd5baf1a9b447b72dc
palewire <b@palewi.re>
2025-04-22 09:44:28 -0400

    Update urls

--- example/project/urls.py
+++ example/project/urls.py
@@ -1,14 +1,11 @@
-# from django.conf import settings
-# from django.contrib import admin
-# from django.views.static import serve
-# from django.conf.urls import url
-# admin.autodiscover()
+# from toolbox import views
+from django.conf import settings
+from django.contrib import admin
+from django.views.static import serve
+from django.urls import re_path
+admin.autodiscover()
 
 
-# urlpatterns = (
-#     url(r'^admin/', admin.site.urls),
-#     url(r'^static/(?P<path>.*)$', serve, {
-#         'document_root': settings.STATIC_ROOT,
-#         'show_indexes': True,
-#     }),
-# )
+urlpatterns = (
+    re_path(r'^admin/', admin.site.urls),
+)
